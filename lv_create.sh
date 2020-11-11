4111e5d7f1e2afa1fb182fd0fb9e471c6bd75d3b
David S. Jackson <deepbsd@yahoo.com>
2020-11-11 12:37:32 -0500

    starting to work now

--- lv_create.sh
+++ lv_create.sh
@@ -6,7 +6,8 @@ SWAP_DEVICE=/dev/sda2
 ROOT_DEVICE=/dev/sda3
 #HOME_DEVICE=/dev/sda4
 VOL_GROUP=arch_vg
-#ROOT_GROUP=root_vg
+LV_ROOT="ArchRoot"
+LV_HOME="ArchHome"
 #HOME_GROUP=home_vg
 
 EFI_SIZE=512M
@@ -21,31 +22,31 @@ lv_create(){
     sgdisk -n 2::+"$SWAP_SIZE" -t 2:8200 -c 2:SWAP "$IN_DEVICE"
     sgdisk -n 3 -t 3:8e00 -c 3:VOLGROUP "$IN_DEVICE"
     # Format the EFI partition
-    mkfs.fat -F32 "$SWAP_DEVICE"
+    mkfs.fat -F32 "$EFI_DEVICE"
     # Format SWAP 
     mkswap "$SWAP_DEVICE"
     swapon "$SWAP_DEVICE"
-    # create the root and home physical volumes
+    # create the physical volumes
     pvcreate "$ROOT_DEVICE"
-    #pvcreate "$HOME_DEVICE"
-    # create the volume groups
-    #vgcreate root_vg "$ROOT_DEVICE"
-    #vgcreate "$VOL_GROUP" "$ROOT_DEVICE" "$HOME_DEVICE"
+    # create the volume group
     vgcreate "$VOL_GROUP" "$ROOT_DEVICE" 
+    
+    # You can extend with 'vgextend' to other devices too
+
     # create the volumes with specific size
-    lvcreate -L "$ROOT_SIZE"  "$VOL_GROUP" -n ArchRoot 
-    lvcreate -L "$HOME_SIZE" "$VOL_GROUP" -n ArchHome 
+    lvcreate -L "$ROOT_SIZE" "$VOL_GROUP" -n "$LV_ROOT"
+    lvcreate -l 100%FREE  "$VOL_GROUP" -n "$LV_HOME"
     # insert the vol group module
     modprobe dm_mod
-    # activate the vol groups
+    # activate the vol group
     vgchange -ay
     # format the volumes
-    mkfs.ext4 /dev/"$VOL_GROUP"/ArchRoot
-    mkfs.ext4 /dev/"$VOL_GROUP"/ArchHome
+    mkfs.ext4 /dev/"$VOL_GROUP"/"$LV_ROOT"
+    mkfs.ext4 /dev/"$VOL_GROUP"/"$LV_HOME"
     # mount the volumes
-    mount /dev/"$VOL_GROUP"/ArchRoot /mnt
+    mount /dev/"$VOL_GROUP"/"$LV_ROOT" /mnt
     mkdir /mnt/home
-    mount /dev/"$VOL_GROUP"/ArchHome /mnt/home
+    mount /dev/"$VOL_GROUP"/"$LV_ROOT" /mnt/home
     # mount the EFI partitions
     mkdir /mnt/boot && mkdir /mnt/boot/efi
     mount /dev/sda1 /mnt/boot/efi
