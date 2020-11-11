38a831d0cbc2570f7e0544d3d9f0258f0a917c7c
David S. Jackson <deepbsd@yahoo.com>
2020-11-11 12:08:10 -0500

    fixing lvcreate syntax

--- lv_create.sh
+++ lv_create.sh
@@ -4,14 +4,14 @@ IN_DEVICE=/dev/sda
 EFI_DEVICE=/dev/sda1
 SWAP_DEVICE=/dev/sda2
 ROOT_DEVICE=/dev/sda3
-HOME_DEVICE=/dev/sda4
+#HOME_DEVICE=/dev/sda4
 VOL_GROUP=arch_vg
-ROOT_GROUP=root_vg
-HOME_GROUP=home_vg
+#ROOT_GROUP=root_vg
+#HOME_GROUP=home_vg
 
 EFI_SIZE=512M
-ROOT_SIZE=14G
-HOME_SIZE=14G
+ROOT_SIZE=12G
+HOME_SIZE=16G
 SWAP_SIZE=2G
 
 lv_create(){
@@ -19,8 +19,7 @@ lv_create(){
     sgdisk -Z "$IN_DEVICE"
     sgdisk -n 1::+"$EFI_SIZE" -t 1:ef00 -c 1:EFI "$IN_DEVICE"
     sgdisk -n 2::+"$SWAP_SIZE" -t 2:8200 -c 2:SWAP "$IN_DEVICE"
-    sgdisk -n 3::+"$ROOT_SIZE" -t 3:8e00 -c 3:ROOT_VOL "$IN_DEVICE"
-    sgdisk -n 4 -t 4:8e00 -c 4:HOME_VOL "$IN_DEVICE"
+    sgdisk -n 3 -t 4:8e00 -c 4:VOLGROUP "$IN_DEVICE"
     # Format the EFI partition
     mkfs.fat -F32 "$SWAP_DEVICE"
     # Format SWAP 
@@ -28,13 +27,14 @@ lv_create(){
     swapon "$SWAP_DEVICE"
     # create the root and home physical volumes
     pvcreate "$ROOT_DEVICE"
-    pvcreate "$HOME_DEVICE"
+    #pvcreate "$HOME_DEVICE"
     # create the volume groups
     #vgcreate root_vg "$ROOT_DEVICE"
-    vgcreate "$VOL_GROUP" "$ROOT_DEVICE" "$HOME_DEVICE"
+    #vgcreate "$VOL_GROUP" "$ROOT_DEVICE" "$HOME_DEVICE"
+    vgcreate "$VOL_GROUP" "$ROOT_DEVICE" 
     # create the volumes with specific size
-    lvcreate -L "$ROOT_SIZE" "$ROOT_DEVICE"
-    lvcreate -L "$HOME_SIZE" "$HOME_DEVICE"
+    lvcreate -L "$ROOT_SIZE"  "$VOL_GROUP" -n ArchRoot  "$ROOT_DEVICE"
+    lvcreate -L "$HOME_SIZE" "$VOL_GROUP" -n ArchHome "$ROOT_DEVICE"
     # insert the vol group module
     modprobe dm_mod
     # activate the vol groups
