ff29ebc33cb6d24a352636af50b3c2f65ecf5926
David S. Jackson <deepbsd@yahoo.com>
2020-12-01 13:05:20 -0500

    working on diskpart.sh, deciding how to work it in the best way

--- diskpart.sh
+++ diskpart.sh
@@ -8,7 +8,7 @@
 #  4) do you want LVM?    5) calculate swap
 
 RAM=()
-DISKS=()
+DISKS=( sdb 400G )
 
 get_disks(){
    for d in $(lsblk | grep disk | awk '{printf "%s\n%s\n",$1,$4}'); do
@@ -17,20 +17,41 @@ get_disks(){
 
    max=${#DISKS[@]}
    for ((n=0;n<$max;n+=2)); do
-        printf "%s\t\t%s\n" "DEVICE" "SIZE"
-        printf "%s\t\t%s\n" ${DISKS[$n]} ${DISKS[(($n+1))]}
+        printf "%s\t\t%s\n" ${DISKS[$n]} ${DISKS[(($n+1))]%.*}
    done
 }
 
 get_swap(){
-    ram=$(free | grep Mem | awk '{print ($2/1000000)*1.4}')
-    swap=${ram%.*}
-    echo "swap:  ${swap}G"
+    hibernate='y'
+    ram=$(free | grep Mem | awk '{print ($2/1000000)}')
+    ram=16
+    half=$(echo "scale=1;$ram*0.5" | bc)
+    half=${half%.*}
+    one_pt_five=$(echo "scale=1;$ram*1.5" | bc)
+    one_pt_five=${one_pt_five%.*}
+    case 1 in
+        $(( $ram <= 2 )) ) [[ "$hibernate" =~ [yY] ]] && swap=$((ram*3)) || swap=$((ram*2)) ;;
+        $(( $ram <= 8 )) ) [[ "$hibernate" =~ [yY] ]] && swap=$((ram*2)) || swap=$ram ;;
+        $(( $ram <= 64 )) ) [[ "$hibernate" =~ [yY] ]] && swap=$((one_pt_five)) || swap=$((half)) ;;
+        $(( $ram > 64 )) ) [[ "$hibernate" =~ [yY] ]] && swap=$((half)) || swap=16  ;;
+    esac
+    echo "$swap"
 }
 
-calculate_swap(){
-echo
+get_root(){
+    if [[ $(get_disks | awk '{print $2}') -le 30 ]]; then
+        size=12
+    elif [[ $(get_disks | awk '{print $2}') -le 50 ]]; then
+        size=20
+    elif [[ $(get_disks | awk '{print $2}') -le 100 ]]; then
+        size=75
+    else
+        size=100
+    fi
+    echo "${size}"
 }
 
-get_disks
-get_swap
+get_disks | awk '{print $2}'
+get_swap 
+#get_root
+
