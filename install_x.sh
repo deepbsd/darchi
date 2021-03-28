546bc31de766fc40a42701a33af4d7b7db86ce78
David S. Jackson <deepbsd@yahoo.com>
2021-03-28 13:37:08 -0400

    making install_x usable and changing it around

--- install_x.sh
+++ install_x.sh
@@ -4,22 +4,26 @@
 
 install_x(){ return 0; }     # return 0 if you want to install X
 
+video_driver='xf86-video-vmware'  # change if not using a virtual machine
+
+declare -A display_mgr=( [dm]='lightdm' [service]='lightdm' )
+
 ## These are packages required for a working Xorg desktop (My preferences anyway)
-BASIC_X=( xorg-server xorg-xinit mesa xorg-twm xterm gnome-terminal xfce4-terminal xorg-xclock "${DESKTOP[@]}" ${DISPLAY_MGR[dm]} firefox )
+basic_x=( xorg-server xorg-xinit mesa xorg-twm xterm gnome-terminal xfce4-terminal xorg-xclock firefox )
 
 ## These are your specific choices for fonts and wallpapers and X-related goodies
-EXTRA_X=( adobe-source-code-pro-fonts cantarell-fonts gnu-free-fonts noto-fonts breeze-gtk breeze-icons oxygen-gtk2 gtk-engine-murrine oxygen-icons xcursor-themes adapta-gtk-theme arc-gtk-theme elementary-icon-theme faenza-icon-theme gnome-icon-theme-extras arc-icon-theme lightdm-gtk-greeter-settings lightdm-webkit-theme-litarvan mate-icon-theme materia-gtk-theme papirus-icon-theme xcursor-bluecurve xcursor-premium archlinux-wallpaper deepin-community-wallpapers deepin-wallpapers elementary-wallpapers )
+extra_x=( adobe-source-code-pro-fonts cantarell-fonts gnu-free-fonts noto-fonts breeze-gtk breeze-icons oxygen-gtk2 gtk-engine-murrine oxygen-icons xcursor-themes adapta-gtk-theme arc-gtk-theme elementary-icon-theme faenza-icon-theme gnome-icon-theme-extras arc-icon-theme lightdm-gtk-greeter-settings lightdm-webkit-theme-litarvan mate-icon-theme materia-gtk-theme papirus-icon-theme xcursor-bluecurve xcursor-premium archlinux-wallpaper deepin-community-wallpapers deepin-wallpapers elementary-wallpapers )
 
-EXTRA_DESKTOPS=( mate mate-extra xfce4 xfce4-goodies i3-gaps i3status i3blocks nitrogen feh rofi dmenu terminator ttf-font-awesome ttf-ionicons )
-
-GOODIES=( htop neofetch screenfetch powerline powerline-fonts powerline-vim )
+goodies=( htop neofetch screenfetch powerline powerline-fonts powerline-vim )
 
 ## -----------  Some of these are included, but it's all up to you...
 xfce_desktop=( xfce4 xfce4-goodies )
 
 mate_desktop=( mate mate-extra )
 
-i3gaps_desktop=( i3-gaps dmenu feh rofi i3status i3blocks nitrogen i3status ttf-font-awesome ttf-ionicons )
+i3gaps_desktop=( i3-gaps terminator alacritty dmenu feh rofi i3status i3blocks nitrogen i3status ttf-font-awesome ttf-ionicons )
+
+extra_desktops=( "${xfce_desktop[@]}" "${mate_desktop[@]}" "${i3gaps_desktop[@]}" )
 
 ## Python3 should be installed by default
 devel_stuff=( git nodejs npm npm-check-updates ruby )
@@ -43,20 +47,22 @@ find_card(){
 }
 
 
+##########################################
+######       INSTALL X       #############
+##########################################
 
-## INSTALL X AND DESKTOP  
 if $(install_x); then
     clear && echo "Installing X and X Extras and Video Driver. Type any key to continue"; read empty
-    arch-chroot /mnt pacman -S "${BASIC_X[@]}"
-    arch-chroot /mnt pacman -S "${EXTRA_X[@]}"
+    arch-chroot /mnt pacman -S "${basic_x[@]}"
+    arch-chroot /mnt pacman -S "${extra_x[@]}"
     your_card=$(find_card)
-    echo "${your_card} and you're installing the $VIDEO_DRIVER driver... (Type key to continue) "; read blah
-    arch-chroot /mnt pacman -S "$VIDEO_DRIVER"
-    arch-chroot /mnt pacman -S "${EXTRA_DESKTOPS[@]}"
-    arch-chroot /mnt pacman -S "${GOODIES[@]}"
+    echo "${your_card} and you're installing the $video_driver driver... (Type key to continue) "; read empty
+    arch-chroot /mnt pacman -S "$video_driver"
+    arch-chroot /mnt pacman -S "${extra_desktops[@]}"
+    arch-chroot /mnt pacman -S "${goodies[@]}"
 
     echo "Enabling display manager service..."
-    arch-chroot /mnt systemctl enable ${DISPLAY_MGR[service]}
+    arch-chroot /mnt systemctl enable ${display_mgr[service]}
     echo && echo "Your desktop and display manager should now be installed..."
     sleep 5
 fi
